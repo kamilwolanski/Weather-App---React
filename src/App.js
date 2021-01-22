8b53a5132a50558226c1711db7634c8f1addbb61
kamilwolanski <kwolanski3@gmail.com>
2021-01-22 13:45:50 +0100

    added height to app container

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ function App() {
 
   return (
     <>
-      <div className="app-container">
+      <div className="app-container" style={{height:windowHeight}}>
         {showPopUp && (
           <PopUp
             setIsSubmitting={setIsSubmitting}
